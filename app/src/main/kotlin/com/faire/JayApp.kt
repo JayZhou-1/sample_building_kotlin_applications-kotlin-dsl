4160f1e69dfcdbe22106a0378bc2c3820016d4d7
Jay Zhou <jay.zhou@faire.com>
2022-06-02 10:23:35 -0400

    use annotation to inject okHttpclient

--- app/src/main/kotlin/com/faire/JayApp.kt
+++ app/src/main/kotlin/com/faire/JayApp.kt
@@ -2,34 +2,48 @@ package com.faire
 
 import com.google.inject.AbstractModule
 import com.google.inject.Guice
+import com.google.inject.Provides
 import faire.BackFill
-import faire.NeverNotify
 import faire.NotifyAfter
 import javax.inject.Inject
+import javax.inject.Qualifier
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 
 @NotifyAfter("2022-01-01")
-class JayApp @Inject constructor(val builder: OkHttpClient.Builder): BackFill() {
+class JayApp : BackFill() {
 
-    fun getHttpClient(): OkHttpClient {
-        return builder.build()
+    @Inject
+    lateinit var okHttpClientTest: OkHttpClientTest
+
+    fun jayAppEntryPoint() {
+//        okHttpClientTest.callSlack();
+        println("=========with interceptor==================")
+        okHttpClientTest.callGoogleWithOkHttpClientInterceptor();
+        println("=============without interceptor==============")
+        okHttpClientTest.callGoogleWithoutOkHttpClientInterceptor()
     }
 
 }
 
 fun main() {
-    val injector = Guice.createInjector(GuiceTestModule())
+    val injector = Guice.createInjector(GuiceModule())
     val jayApp = injector.getInstance(JayApp::class.java)
-    val client = jayApp.getHttpClient();
-    println("client = ${client.callTimeoutMillis}")
+    jayApp.jayAppEntryPoint()
 }
 
-@NeverNotify
-class GuiceTestModule : AbstractModule() {
 
-}
+class GuiceModule : AbstractModule() {
+    @Provides
+    @WithTransactionInterceptor
+    fun okHttpClientWithInjector(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(Interceptor { chain ->
+            println("it has interceptor")
+            chain.proceed(chain.request())
+        }).build()
+    }
 
-@NotifyAfter(date = "2022-07-04")
-class JayApp2() : BackFill(){
-
+    @Qualifier
+    @Retention(AnnotationRetention.RUNTIME)
+    annotation class WithTransactionInterceptor
 }
---
+++ app/src/main/kotlin/com/faire/OkHttpClientTest.kt
@@ -0,0 +1,59 @@
+package com.faire
+
+import javax.inject.Inject
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+
+//class OkHttpClientTest @Inject constructor( @GuiceModule.WithTransactionInterceptor val okHttpClientWithInterceptor: OkHttpClient){
+class OkHttpClientTest {
+
+    @Inject
+    @field:GuiceModule.WithTransactionInterceptor
+    private lateinit var okHttpClientWithInterceptor: OkHttpClient
+
+    @Inject
+    private lateinit var okHttpClient: OkHttpClient
+
+    fun callSlack() {
+        var slackToken = System.getenv("CODE_CLEANUP_BOT_SLACK_TOKEN")
+
+        val formBody: RequestBody = FormBody.Builder()
+            .add("channel", "#testing")
+            .add("text", "jayMessage from raw ${this.javaClass}")
+            .build()
+        val request = Request.Builder()
+            .url("https://slack.com/api/chat.postMessage")
+            .addHeader("Authorization", "Bearer $slackToken")
+            .post(formBody)
+            .build()
+
+        val call = okHttpClient.newCall(request);
+        val response = call.execute();
+        println("response = ${response}")
+        okHttpClient.connectionPool.evictAll()
+    }
+
+    fun callGoogleWithoutOkHttpClientInterceptor() {
+        val request = Request.Builder()
+            .url("https://google.com")
+            .build()
+
+        val call = okHttpClient.newCall(request);
+        val response = call.execute();
+        println("response = ${response}")
+//        okHttpClient.connectionPool.evictAll()
+    }
+
+    fun callGoogleWithOkHttpClientInterceptor() {
+        val request = Request.Builder()
+            .url("https://google.com")
+            .build()
+
+        val call = okHttpClientWithInterceptor.newCall(request);
+        val response = call.execute();
+        println("response = ${response}")
+//        okHttpClientWithInterceptor.connectionPool.evictAll()
+    }
+}
